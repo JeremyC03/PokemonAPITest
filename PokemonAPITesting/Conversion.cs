1c352944a10cdbd4facaccc9fe0a962bc3fb6f00
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-08-07 14:58:17 -0400

    Works on Console App

--- PokemonAPITesting/Conversion.cs
+++ PokemonAPITesting/Conversion.cs
@@ -8,6 +8,15 @@ namespace PokemonAPITesting
 {
     public class Conversion
     {
-
+        public static double FootConversion(double decimeter)
+        {
+            double answer = decimeter / 3.048;
+            return answer;
+        }
+        public static double PoundConversion(double hectogram)
+        {
+            double answer = hectogram / 4.536;
+            return answer;
+        }
     }
 }
--- PokemonAPITesting/PokemonAPITest.cs
+++ PokemonAPITesting/PokemonAPITest.cs
@@ -20,73 +20,87 @@ namespace PokemonAPITesting
             var pokemonInput = Console.ReadLine();
             Console.WriteLine();
 
-            //URL endpoint
-            string pokemonURL = $"https://pokeapi.co/api/v2/pokemon/{pokemonInput.ToLower()}";
-            //GET request
-            string pokemonResponse = client.GetStringAsync(pokemonURL).Result;
-            //Parse Object/Array
-            var pokemonObject = JObject.Parse(pokemonResponse);
+            //While loop to get valid input
+            bool conditional = true;
 
-            //Write to console
-            Console.WriteLine("Species:");
-            Console.WriteLine($"{pokemonObject["species"]["name"]}");
-            Console.WriteLine();
-
-            Console.WriteLine($"ID:");
-            Console.WriteLine($"{pokemonObject["id"]}");
-            Console.WriteLine();
-
-            //Foreach loop to write multiple answers if pokemon has more than 1 answer
-            Console.WriteLine("Type:");
-            foreach (var type in pokemonObject["types"])
+            while (conditional)
             {
-                Console.WriteLine($"{type["type"]["name"]}");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Height:");
-            Console.WriteLine($"{pokemonObject["height"]} decimeters");
-            Console.WriteLine();
-
-            Console.WriteLine("Weight:");
-            Console.WriteLine($"{pokemonObject["weight"]} hectograms");
-            Console.WriteLine();
-
-            //If statement if pokemon doesn't have answer
-            Console.WriteLine("Possible held items:");
-
-            JToken dataToken;
-
-            if (pokemonObject.TryGetValue("held_items", out dataToken) && dataToken is JArray array && array.Count == 0)
-            {
-                Console.WriteLine("None");
-            }
-            else
-            {
-                foreach (var items in pokemonObject["held_items"])
+                //Try-Catch if input is incorrect
+                try
                 {
-                    Console.WriteLine($"{items["item"]["name"]}");
+                    //URL endpoint
+                    string pokemonURL = $"https://pokeapi.co/api/v2/pokemon/{pokemonInput.ToLower()}";
+                    //GET request
+                    string pokemonResponse = client.GetStringAsync(pokemonURL).Result;
+                    //Parse Object/Array
+                    var pokemonObject = JObject.Parse(pokemonResponse);
+
+                    //Write to console
+                    Console.WriteLine("Species:");
+                    Console.WriteLine($"{pokemonObject["species"]["name"]}");
+                    Console.WriteLine();
+
+                    Console.WriteLine($"ID:");
+                    Console.WriteLine($"{pokemonObject["id"]}");
+                    Console.WriteLine();
+
+                    //Foreach loop to write multiple answers if pokemon has more than 1 answer
+                    Console.WriteLine("Type:");
+                    foreach (var type in pokemonObject["types"])
+                    {
+                        Console.WriteLine($"{type["type"]["name"]}");
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Height:");
+                    Console.WriteLine($"{pokemonObject["height"]} decimeters");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Weight:");
+                    Console.WriteLine($"{pokemonObject["weight"]} hectograms");
+                    Console.WriteLine();
+
+                    //If statement if pokemon doesn't have answer
+                    Console.WriteLine("Possible held items:");
+                    JToken dataToken;
+                    if (pokemonObject.TryGetValue("held_items", out dataToken) && dataToken is JArray array && array.Count == 0)
+                    {
+                        Console.WriteLine("None");
+                    }
+                    else
+                    {
+                        foreach (var items in pokemonObject["held_items"])
+                        {
+                            Console.WriteLine($"{items["item"]["name"]}");
+                        }
+
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Possible Abilities:");
+                    foreach (var ability in pokemonObject["abilities"])
+                    {
+                        Console.WriteLine($"{ability["ability"]["name"]}");
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Base Stats:");
+                    foreach (var stats in pokemonObject["stats"])
+                    {
+                        Console.WriteLine($"{stats["stat"]["name"]}: {stats["base_stat"]} ");
+                    }
+                    Console.WriteLine();
+
+                    conditional = false;
                 }
-
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Please enter a pokemon name");
+                    string retryInput = Console.ReadLine();
+                    pokemonInput = retryInput;
+                    Console.WriteLine();
+                }            
             }
-            Console.WriteLine();
-
-            Console.WriteLine("Possible Abilities:");
-            foreach (var ability in pokemonObject["abilities"])
-            {
-                Console.WriteLine($"{ability["ability"]["name"]}");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Base Stats:");
-            foreach (var stats in pokemonObject["stats"])
-            {
-                Console.WriteLine($"{stats["stat"]["name"]}: {stats["base_stat"]} ");
-            }
-            Console.WriteLine();
-
         }
-
-
     }
 }
